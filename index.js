d357d4104f39e85b956328dfb6215483410ee9f7
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-12-29 08:23:25 +0100

    standard --fix. Refactoring. Updates package-lock (patch)

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-const readFileSync = require('fs').readFileSync
+const { readFile } = require('fs').promises
 const md = require('markdown-it')()
-const { parse } = require('url')
 const { send } = require('micro')
 const renderPage = require('./lib/render-page')
 const getAnswer = require('./lib/get-answer')
 
 module.exports = async (request, response) => {
-  const { pathname, query } = await parse(request.url, true)
-  if (pathname === '/json' || pathname === '/html') {
-    const results = { description: getAnswer(query.mode || 'normal') }
-
-    if (pathname === '/json') {
+  const { url } = request
+  const query = await request.query
+  const mode = query ? query.mode : 'normal'
+  const isJson = /json/.test(url)
+  const isHtml = /html/.test(url)
+  if (isJson || isHtml) {
+    const results = { description: getAnswer(mode) }
+    if (isJson) {
       response.setHeader('Access-Control-Allow-Origin', '*')
       response.setHeader('Access-Control-Allow-Methods', 'GET')
       send(response, 200, results)
-    } else if (pathname === '/html') {
+    } else if (isHtml) {
       response.setHeader('Content-Type', 'text/html; charset=utf-8')
       send(response, 200, renderPage(results))
     }
   } else {
-    const readme = readFileSync(`${__dirname}/README.md`, 'utf-8')
+    const readme = await readFile(`${__dirname}/README.md`, 'utf-8')
     send(response, 200, md.render(readme))
   }
 }
